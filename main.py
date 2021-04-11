dd0c9da844c79b2933c749da2982435ec776e72e
Carlos Del Carpio <cdeclarp@uncc.edu>
2021-04-10 20:41:58 -0400

    Created new html pages for different link redirection. Created css stylesheet. Created initail methods to navigate website.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 from flask import Flask
-from flask import render_template
+from flask import render_template, url_for
+from flask import redirect
 # from flask import request
-# from flask import redirect, url_for
 
 
 app = Flask(__name__)  # create an app
@@ -12,6 +12,21 @@ app = Flask(__name__)  # create an app
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/events')
+def events():
+    return render_template('events.html')
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
